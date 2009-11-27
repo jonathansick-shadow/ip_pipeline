0ec463e88588ca9d554ceeeef69775f773358f12
rhl <rhl@git.lsstcorp.org>
2009-11-27 22:33:33 +0000

    Initial version of crRejection stage

---
+++ python/lsst/ip/pipeline/__init__.py
@@ -0,0 +1,1 @@
+from crRejectStage import *
---
+++ python/lsst/ip/pipeline/crRejectStage.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+import math
+import lsst.pex.harness.stage as harnessStage
+
+from lsst.pex.logging import Log
+
+import lsst.pex.policy as pexPolicy
+import lsst.afw.detection as afwDet
+import lsst.afw.image as afwImg
+import lsst.afw.math as afwMath
+import lsst.pex.exceptions as pexExcept
+import lsst.meas.algorithms as measAlg
+
+import lsst.afw.display.ds9 as ds9
+import lsst.afw.display.utils as displayUtils
+
+try:
+    type(display)
+except NameError:
+    display = False
+
+class CrRejectStageParallel(harnessStage.ParallelProcessing):
+    """
+    Description:
+        This stage wraps estimating and possibly subtracting cosmic rays from an exposure
+        on the clipboard.        
+
+    Policy Dictionary:
+    lsst/ip/pipeline/policy/CrRejectStageDictionary.paf
+
+    Clipboard Input:
+    - Calibrated science Exposure(s) (without background)
+    - a PSF may be specified by policy attribute inputPsfKey. Alternatively, the
+      stage's policy may request that a psf be constructed, by providing the
+      psfPolicy attribute.
+
+    ClipboardOutput:
+    - Exposure with CRs removed. Key specified
+        by policy attribute 'crSubtractedExposureKey'
+    - nCR The number of CRs detected
+    - PSF: the psf used to smooth the exposure before detection 
+        Key specified by policy attribute 'psfKey'
+    """
+    def setup(self):
+        self.log = Log(self.log, "CrRejectStage - parallel")
+
+        policyFile = pexPolicy.DefaultPolicyFile("ip_pipeline", "CrRejectStageDictionary.paf", "policy")
+        defPolicy = pexPolicy.Policy.createPolicy(policyFile, policyFile.getRepositoryPath())
+
+        if self.policy is None:
+            self.policy = pexPolicy.Policy()
+        self.policy.mergeDefaults(defPolicy)
+
+        if True:                        # workaround #1035
+            dfile = pexPolicy.DefaultPolicyFile("meas_utils", 
+                                                "CrRejectDictionary.paf", "policy")
+            
+            defpolicy = pexPolicy.Policy.createPolicy(dfile, dfile.getRepositoryPath())
+            tmp = pexPolicy.Policy()
+            tmp.mergeDefaults(defpolicy)
+            self.policy.add("crRejectPolicy", tmp)
+
+        self.crRejectPolicy = self.policy.get("crRejectPolicy")
+
+    def process(self, clipboard):
+        """
+        Detect CRs in the worker process
+        """
+        self.log.log(Log.INFO, "Detecting CRs in process")
+        
+        #grab exposure from clipboard
+        exposure = clipboard.get(self.policy.getString("inputKeys.exposure"))
+
+        self.crRejectPolicy.set('gain', exposure.getMetadata().get('GAIN'))
+        # Set backwards compatible names; should fix meas/algorithms
+        self.crRejectPolicy.set('e_per_dn', self.crRejectPolicy.get('gain'))
+        self.crRejectPolicy.set('min_sigma', self.crRejectPolicy.get('minSigma'))
+
+        mi = exposure.getMaskedImage()
+
+        defaultFwhm = self.policy.get('defaultFwhm')
+        psf         = measAlg.createPSF('DoubleGaussian', 0, 0, defaultFwhm/(2*math.sqrt(2*math.log(2))))
+
+        bg = afwMath.makeStatistics(mi, afwMath.MEDIAN).getValue()
+        crs         = measAlg.findCosmicRays(mi, psf, bg, self.crRejectPolicy, False)
+        nCR = len(crs)
+
+        #output products
+        clipboard.put("nCR", nCR)
+        clipboard.put(self.policy.get("outputKeys.crSubtractedExposure"), exposure)
+        
+class CrRejectStage(harnessStage.Stage):
+    parallelClass = CrRejectStageParallel
+
