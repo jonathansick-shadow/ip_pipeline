98be106903fb73838995402f04a56dba05c789cb
buildbot <buildbot@git.lsstcorp.org>
2009-12-01 16:41:50 +0000

    ip_pipeline unit test artifacts

--- tests/.tests/DetectTest.py
+++
@@ -1,8 +0,0 @@
-tests/DetectTest.py
-
-afwdata is not setting up; skipping test
-.
-----------------------------------------------------------------------
-Ran 1 test in 0.000s
-
-OK
--- tests/.tests/SourceToDiaSourceStageTest.py
+++
@@ -1,8 +0,0 @@
-tests/SourceToDiaSourceStageTest.py
-
-SimpleStageTester: Executing in process
-..
-----------------------------------------------------------------------
-Ran 2 tests in 0.006s
-
-OK
