962db78fcedc6b8765920642d4319156d73205ff
krughoff <krughoff@git.lsstcorp.org>
2010-04-29 21:19:23 +0000

    Ccd defect stages

---
+++ python/lsst/ip/pipeline/isrCcdDefectStage.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+import math
+import lsst.pex.harness.stage as harnessStage
+
+from lsst.pex.logging import Log
+
+import lsst.pex.policy as pexPolicy
+import lsst.ip.isr as ipIsr
+import lsst.afw.cameraGeom as cameraGeom
+
+class IsrCcdDefectStageParallel(harnessStage.ParallelProcessing):
+    """
+    Description:
+
+    Policy Dictionary:
+
+    Clipboard Input:
+
+    ClipboardOutput:
+    """
+    def setup(self):
+        self.log = Log(self.log, "CcdDefect -- Parallel")
+
+        policyFile = pexPolicy.DefaultPolicyFile("ip_pipeline",
+                "IsrCcdDefectStageDictionary.paf", "policy")
+        defPolicy = pexPolicy.Policy.createPolicy(policyFile, policyFile.getRepositoryPath())
+
+        if self.policy is None:
+            self.policy = pexPolicy.Policy()
+        self.policy.mergeDefaults(defPolicy)
+
+    def process(self, clipboard):
+        """
+        """
+        self.log.log(Log.INFO, "Doing CCD defect mask.")
+        
+        #grab exposure from clipboard
+        exposure = clipboard.get(self.policy.getString("inputKeys.ccdExposure"))
+        defectList = cameraGeom.cast_Ccd(exposure.getDetector()).getDefects()
+        ipIsr.maskBadPixelsDef(exposure, defectList,
+            self.policy.getDouble("parameters.defaultFwhm"), interpolate=False, maskName='BAD')
+        #output products
+        clipboard.put(self.policy.get("outputKeys.defectMaskedCcdExposure"),
+                exposure)
+        
+class IsrCcdDefectStage(harnessStage.Stage):
+    parallelClass = IsrCcdDefectStageParallel
+
